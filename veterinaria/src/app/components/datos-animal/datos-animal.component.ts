ba60182ab04a302aaccf0fc86e6e2a1895dbc36e
julianaMarin12 <142064728+julianaMarin12@users.noreply.github.com>
2024-11-19 23:27:28 -0500

    Try to connect back

--- veterinaria/src/app/components/datos-animal/datos-animal.component.ts
+++ veterinaria/src/app/components/datos-animal/datos-animal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-datos-animal',
   standalone: true,
@@ -8,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './datos-animal.component.scss'
 })
 export class DatosAnimalComponent {
+  pets: any[] = []; // Aquí se almacenarán los datos de las mascotas
+
+  constructor() {}
+
+  ngOnInit() {
+    // Llama al backend usando fetch
+    fetch('http://localhost:8080/proyecto/pets') // Cambia la URL si es necesario
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Error al obtener los datos');
+        }
+        return response.json(); // Convierte la respuesta a JSON
+      })
+      .then((data) => {
+        this.pets = data; // Asigna los datos a la variable 'pets'
+      })
+      .catch((error) => {
+        console.error('Error al obtener las mascotas:', error);
+      });
+  }
 
 }
--- veterinaria/src/app/components/mi-cuenta/mi-cuenta.component.ts
+++ veterinaria/src/app/components/mi-cuenta/mi-cuenta.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './mi-cuenta.component.scss'
 })
 export class MiCuentaComponent {
+  
 
 }
